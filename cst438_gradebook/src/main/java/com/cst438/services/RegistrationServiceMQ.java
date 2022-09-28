a762221686f3bf36493a63fbc528c610562d55c1
hulebe <tst87438@gmail.com>
2022-09-27 18:43:36 -0700

    Assignment 4 - Integrate gradebook & registration services

--- cst438_gradebook/src/main/java/com/cst438/services/RegistrationServiceMQ.java
+++ cst438_gradebook/src/main/java/com/cst438/services/RegistrationServiceMQ.java
@@ -1,11 +1,12 @@
 package com.cst438.services;
 
-
 import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.server.ResponseStatusException;
 
 import com.cst438.domain.Course;
 import com.cst438.domain.CourseDTOG;
@@ -44,7 +45,19 @@ public class RegistrationServiceMQ extends RegistrationService {
 	@Transactional
 	public void receive(EnrollmentDTO enrollmentDTO) {
 		
-		//TODO  complete this method in homework 4
+		// homework 4
+		Course c = courseRepository.findById(enrollmentDTO.course_id).orElse(null);
+		if (c==null) {
+			throw new ResponseStatusException( HttpStatus.BAD_REQUEST, "Course id not found.");
+		}
+		
+		Enrollment e = new Enrollment();
+		e.setCourse(c);
+		e.setStudentName(enrollmentDTO.studentName);
+		e.setStudentEmail(enrollmentDTO.studentEmail);
+		
+		enrollmentRepository.save(e);
+		
 		
 	}
 
@@ -52,8 +65,9 @@ public class RegistrationServiceMQ extends RegistrationService {
 	@Override
 	public void sendFinalGrades(int course_id, CourseDTOG courseDTO) {
 		 
-		//TODO  complete this method in homework 4
-		
+		// homework 4
+		this.rabbitTemplate.convertAndSend(registrationQueue.getName(), courseDTO);
+		System.out.println("Final grade sent.");
 	}
 
 }
